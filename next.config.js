d3bb02e3c8dae8d9f20788dfc7472c3de4fbe25c
Lemon Studios <lmnstds@gmail.com>
2026-03-13 23:39:11 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/Vikramaditya-Portfolio',
+  assetPrefix: '/Vikramaditya-Portfolio/',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
